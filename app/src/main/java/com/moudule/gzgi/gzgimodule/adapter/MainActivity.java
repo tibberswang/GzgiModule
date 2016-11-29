c99fbd6c5c8f5940b60805f7b69d47522e0e4202
= <=>
2016-11-29 11:10:52 +0800

    增加了各类package

---
+++ app/src/main/java/com/moudule/gzgi/gzgimodule/adapter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.moudule.gzgi.gzgimodule.adapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.moudule.gzgi.gzgimodule.R;
+
+class AA extends AppCompatActivity {
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
