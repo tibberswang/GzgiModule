1b0f889196da3f46a15010fc7eb21012b25fc770
= <=>
2016-11-29 10:53:58 +0800

    第二次修改

--- app/src/main/java/com/moudule/gzgi/gzgimodule/MainActivity.java
+++ app/src/main/java/com/moudule/gzgi/gzgimodule/MainActivity.java
@@ -2,12 +2,14 @@ package com.moudule.gzgi.gzgimodule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
